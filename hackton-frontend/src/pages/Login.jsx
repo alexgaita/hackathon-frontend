d604d8614b1b81af2af03f3d68f6667bc2f90b66
CatalinTiu <catalintium@gmail.com>
2023-09-28 12:02:44 +0300

    Added LoginPage and material ui

--- hackton-frontend/src/pages/Login.jsx
+++ hackton-frontend/src/pages/Login.jsx
@@ -1,5 +1,101 @@
+import React, { useState } from 'react';
+import useLocalStorage from "../hooks/useLocalStorage"
+import {
+  Container,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+  Alert,
+  AlertTitle,
+  Box
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+import './Login.css'; 
+
+
 const Login = () => {
-  return <div>LogIn</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [error, setError] = useState('');
+  const localStorage = useLocalStorage();
+
+  const handleLogin = () => {
+    // replace with response from backend call
+    if (username === 'user' && password === 'pass') {
+      setLoggedIn(true);
+      localStorage.addItem('username', username);
+      setError('');
+    } else {
+      setError('Invalid username or password');
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.deleteItem('username');
+    setLoggedIn(false);
+  };
+
+  return (
+    <Box className="background-image">
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={3} sx={{ padding: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <LockOutlinedIcon color="primary" sx={{ fontSize: '2rem' }} />
+        {error && (
+          <Alert severity="error" sx={{ marginTop: 2 }}>
+            <AlertTitle>Error</AlertTitle>
+            {error}
+          </Alert>
+        )}
+        {loggedIn ? (
+          <Button variant="contained" color="secondary" onClick={handleLogout}>
+            Logout
+          </Button>
+        ) : (
+          <Box>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="button"
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+          </Box>
+        )}
+      </Paper>
+    </Container>
+    </Box>
+  );
 };
 
 export default Login;
